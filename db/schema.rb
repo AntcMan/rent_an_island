3104d5623cd75cb24206e0d1eda7746ec1b529f6
Anthony Marion <anthonycmarion@gmail.com>
2023-02-27 16:12:09 +1100

    Added Island model, updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_27_045652) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_27_050955) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "islands", force: :cascade do |t|
+    t.string "name"
+    t.integer "capacity"
+    t.text "description"
+    t.string "location"
+    t.integer "price"
+    t.boolean "availability"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_islands_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -28,4 +41,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_27_045652) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "islands", "users"
 end

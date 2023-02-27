1390f38ed37bdadb303ffa01c5b5ad4fc61615fa
Anthony Marion <anthonycmarion@gmail.com>
2023-02-27 17:26:05 +1100

    Completed user model, added user views

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  # root to: "pages#home"
-  root to: "islands#index"
+  root to: "pages#home"
+  # root to: "islands#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")

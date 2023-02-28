e3dfcecd6d3bff7da16ac54e97c46cb862d60626
Anthony Marion <anthonycmarion@gmail.com>
2023-02-28 11:52:43 +1100

    Updated Island controller

--- app/controllers/islands_controller.rb
+++ app/controllers/islands_controller.rb
@@ -1,10 +1,37 @@
 class IslandsController < ApplicationController
   def new
+    @island = Island.new
+  end
+
+  def create
+    @island = Island.new(island_params)
+    if @island.save
+      redirect_to island_show_path
+    else
+      render :new
+    end
+  end
+
   end
 
   def edit
+    @island = Island.find(params[:id])
   end
 
   def show
+    @island = Island.find(params[:id])
   end
+
+  private
+
+  def island_params
+    params.required(:island).permit(:name, :capacity, :description, :price, :location, :availability, :photo)
+  end
+
 end
+
+# create migration adding "photo" to island table - DONE
+# db:migrate - DONE
+# complete new/.create/edit/show method - DONE
+# upload function + install gem to upload photo to cloudinary
+# set up views/islands/

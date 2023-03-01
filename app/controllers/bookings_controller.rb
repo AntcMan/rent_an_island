140c48b0e38de78dbf1e0979ecf11e138d28ee3a
Johan Binst <johan.binst@gmail.com>
2023-03-01 12:07:40 +1100

    booking feature added

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,34 @@
+class BookingsController < ApplicationController
+  def new
+    # @island = Island.find(params[:island_id])
+    # @new = Booking.new()
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  def create
+    booking = Booking.new(booking_params)
+    booking.island = Island.find(params[:island_id])
+    # booking.total_price = (params(:check_out_date) - params(:check_in_date)) * booking.island.price
+    booking.user = current_user
+    if booking.save
+      # flash.now[:notice] = 'Booking done'
+      redirect_to booking_path(booking)
+    else
+      # flash.now[:alert] = 'Booking not valid'
+      # redirect_to island_path(Island.find(params[:island_id]))
+      redirect_to new_user_session_path
+    end
+  end
+
+  def edit
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:check_in_date, :check_out_date)
+  end
+end

4decc291cf4a0ed9be396663d940f9a39f714406
tan nguyen <saigoncuoinam@gmail.com>
2023-03-02 12:44:32 +1100

    seed with island description and prefix

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,13 +24,16 @@ p "Creating 10 islands ..."
 10.times do |i|
   photo = URI.open("https://source.unsplash.com/random/1920x1080/?island")
   user = User.all.sample
-  suffixes = ["quite place", "island", "resort", "retreat"]
+  suffixes = ["Quiet Cove", "Paradise Island", "Secluded Resort", "Tropical Retreat", "Exotic Oasis", "Sandy Shores", "Beachside Bungalow", "Isolated Haven", "Island Escape", "Remote Hideaway"]
   name = "#{user.first_name}'s #{suffixes.sample}"
   capacity = (10..100).to_a.sample
-  description = Faker::Lorem.sentences(number: 5).join(" ")
   location = Faker::Fantasy::Tolkien.location
   price = (10000..100000).to_a.sample
-  attributes_hash = {
+  island_descriptions = ["A tropical paradise with stunning beaches, lush forests, and vibrant nightlife.", "An island with a buzzing party scene, beautiful beaches, and plenty of water sports.", "A laid-back island with gorgeous beaches, natural beauty, and a relaxed vibe.", "with crystal-clear waters, white-sand beaches, and a strong cultural heritage.", "with some of the most beautiful beaches and waters in the world, perfect for diving and snorkeling.", "An island nation with stunning beaches, lush rainforests, and a unique Melanesian culture.", "You are close to a group of 15 islands with turquoise waters, white sand beaches, and a relaxed Polynesian vibe.", "An archipelago with coral reefs, clear waters, and French-influenced cuisine and culture.", "A UNESCO World Heritage site with breathtaking natural beauty, stunning beaches, and diverse wildlife.", "with white sand beaches, clear waters, and a range of outdoor activities."]
+
+  description = island_descriptions.sample
+  # description = Faker::Lorem.sentences(number: 5).join(" ")
+    attributes_hash = {
     name: name,
     capacity: capacity,
     description: description,
